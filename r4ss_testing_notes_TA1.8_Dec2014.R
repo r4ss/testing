76b871ff5a04cfb1ae838ad11c494ebfcee4191c
Ian Taylor <ian.taylor@noaa.gov>
2015-01-07 11:14:53 -0800

    a few additions to Ian's testing notes for himself

--- r4ss_testing_notes_TA1.8_Dec2014.R
+++ r4ss_testing_notes_TA1.8_Dec2014.R
@@ -1,8 +1,13 @@
 library(r4ss)
 update_r4ss_files(local='c:/github/r4ss/R/')
 
+devtools::install_github("r4ss/r4ss", ref="TA1.8testing")
+
 # sablefish model
-sab <- SS_output('C:/ss/sablefish/sablefish_2011_test_run',forecast=FALSE)
+sab <- SS_output('C:/ss/Sablefish/Sablefish2011/2011 sablefish test run')
+SS_plots(sab, png=TRUE, aalresids=TRUE)
+
+#sab <- SS_output('C:/ss/sablefish/sablefish_2011_test_run',forecast=FALSE)
 graphics.off(); rm(.SavedPlots)
 SS_plots(sab,plot=16,fleets=5)
 SS_plots(sab,plot=16,fleets=5,png=TRUE)
@@ -19,3 +24,5 @@ SSplotComparisons(s.summarize)
 
 SSplotComparisons(s.summarize, uncertainty=c(FALSE,TRUE,FALSE),subplot=1)
 SSplotComparisons(s.summarize, uncertainty=2,subplot=1)
+
+
